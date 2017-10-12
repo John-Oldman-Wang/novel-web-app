36d904f63437f2c116464af92af342ab61a6046a
nokisnojok <mag1c_shen_09@hotmail.com>
2017-10-12 16:34:54 +0800

    crawler is complete,neead mongo database  api
    crawler is complete,neead mongo database  api

--- crawler.js
+++ crawler.js
@@ -1,27 +1,50 @@
 const cheerio = require('cheerio')
 const request = require('request')
 const http = require('http')
-const path = require('path')
+var path = require('path')
 const url=require('url')
 const iconv=require('iconv-lite')
 
-var hostname='www.biqudu.com'
+var hostname='http://www.biqudu.com'
 var urls=['http://www.biqudu.com/']
+var novel_urls=[]
+for(var i=1;i<111111;i++){
+    var path='/1_'+i
+    novel_urls.push(url.resolve(hostname,path))
+    if(i==10){
+        console.log(novel_urls)
+    }
+}
+var json=[]
+req(novel_urls.shift(),function cb(err,res,buffer){
+    if(!err&&res.statusCode=='200'){
+        json.push(res.request.uri.href)
+        if(json.length==10){
+            console.log(json)
+        }
+    }else if(err){
+        console.log(res.request.ui.href,':',err)
+    }else{
+        console.log(res.request.ui.href,':',res.statusCode)
+    }
+    if(novel_urls.length==0){
+        return
+    }
+    req(novel_urls.shift(),cb)
+})
 
-// request('http://www.biquge5200.com/',(err,res,body)=>{
-//     console.log(`err:${err}`)
-//     console.log(`statuscode:${res&&res.statusCode}`)
-//     console.log(Buffer.isBuffer(body))
-//     //console.log(iconv.decode(buf,'gbk'))
-//     //console.log(`body:${body}`)
-//     //get_url(body)
-// })
 function req(url,callbake){
+    console.log('url:',url)
     request(url,function(err,res,body){
-        var content_type=res.headers['content-type']
-        var charset=content_type.replace(/^.*charset=(.*)$/,'$1')
+        if('headers' in res){
+            var content_type=res.headers['content-type']
+            var charset=content_type.replace(/^.*charset=(.*)$/,'$1')
+        }else{
+            console.log('headers not in res')
+        }
+        
+        
         callbake&&callbake(err,res,res.buffer)
-        //console.log(iconv.decode(res.buffer,'gbk'))
     }).on('response',function(res){
         var buf=new Buffer('')
         res.on('data',function(chunk){
@@ -32,27 +55,21 @@ function req(url,callbake){
         })
     })
 }
-
-// .on('data',function(chunk){
-//     buf1=Buffer.concat([buf1,chunk])
-// }).on('end',function(){
-//     console.log(iconv.decode(buf1,'gbk'))
-// })
-// http.get('http://www.biquge5200.com/',function(res){
-//     var buf=new Buffer('')
-//     res.on('data',function(chunk){
-//         buf=Buffer.concat([buf,chunk])
-//     })
-//     res.on('end',function(){
-//         console.log(iconv.decode(new Buffer(buf.toString()),'gbk'))
-//     })
-// })
-function get_url(body){
+function getCharsetFromResHeaders(headers){
+    var content_type=headers['content-type']
+    if(content_type.indexOf('charset')==-1)
+        return 'utf8'
+    var charset=content_type.replace(/^.*charset=(.*)$/,'$1')
+    return charset
+}
+function filterForUrl(body,href){
     var $=cheerio.load(body)
     var url_set=new Set()
     $('a').each(function(){
-        url_set.add($(this).attr('href')) 
+        url_set.add(url.resolve(href,$(this).attr('href')))
+        console.log(url.resolve(href,$(this).attr('href')),'<-',$(this).attr('href'))
     })
     var url_arr=[...url_set]
-    console.log(url_arr)
+    return url_arr
 }
+
--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const { URL } = require('url');
+const path=require('path')
+const url=require('url')
 const request = require('request')
 var hostname='www.biqudu.com'
 const myURL = new URL('https://www.biqudu.com/asdakdjasd/');
 request('/asdasd',function(err){
-    console.log('err',err)
+    //console.log('err',err)
 })
+
+
+console.log(url.resolve('https://www.baidu.com/first/second','https://www.baidu.com/join/joinwaht'))
 console.log('asd')
