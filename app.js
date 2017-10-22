677216ff12dad64b86670b091d1d2f8c148853eb
wanglin <mag1c_shen_09@foxmail.com>
2017-10-22 14:12:44 +0800

    add some route to show novels data
    add some route to show novels data

--- app.js
+++ app.js
@@ -13,15 +13,38 @@ app.set('view engine','ejs')
 app.set('views','./views')
 app.get('/',function(req,res,next){
     res.setHeader('Cache-Control','no-cache')
-    Novel.fetch(function(err,novels){
+    Novel.find({}).limit(100).exec(function(err,novels){
         if(err){
-            next&&next('err')
+            next&&next(err)
             return
         }
         res.render('index',{
             novels:novels
         })
     })
+})
+app.get('/novel',function(req,res,next){
+    //console.log(next.toString())
+    var n=parseInt(req.query.page)
+    Novel.find({}).skip(n*100).limit(100).exec(function(err,novels){
+        if(err){
+            next&&next(err)
+            return
+        }
+        res.render('index',{
+            novels:novels
+        })
+    })
+})
+app.get('/search',function(req,res,next){
+    res.render('search')
+})
+app.get('/s_novel',function(req,res){
+    var key=req.query.key
+    
+})
+app.get('/timeout',function(req,res){
+    res.setHeader('countent-type','text/html')
 })
 app.use(function(err,req,res,next){
     console.log(err)
