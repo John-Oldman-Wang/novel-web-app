642f0a5ca086a0944ba41c1977a70515f0ac527f
wanglin <mag1c_shen_09@foxmail.com>
2017-12-04 13:53:39 +0800

    clear notes
    clear notes

--- crawler/httpget.js
+++ crawler/httpget.js
@@ -1,31 +1,8 @@
 var request=require('request')
 
 
-/*function req(url,callbake){
-    console.log(`start get ${url}`)
-    request(url,{timeout:5000},function(err,res,body){
-        console.log('do cb')
-        if(!!res&&'headers' in res){
-            var content_type=res.headers['content-type']
-            var charset=content_type.replace(/^.*charset=(.*)$/,'$1')
-        }else{
-            console.log('headers not in res')
-        }
-        callbake&&callbake.bind(this)(err,res,res&&'buffer' in res?res.buffer:body)
-    }).on('response',function(res){
-        var buf=new Buffer('')
-        res.on('data',function(chunk){
-            buf=Buffer.concat([buf,chunk])
-        })
-        res.on('end',function(){
-            res.buffer=buf
-        })
-    })
-}*/
 function req(url,callbake){
-    //console.log(`start get ${url}`)
     request(url,{timeout:5000},function(err,res,body){
-        //console.log('do cb')
         callbake&&callbake.bind(this)(err,res,!!res&&'buffer' in res?res.buffer:body)
     }).on('response',function(res){
         var buf=new Buffer('')
