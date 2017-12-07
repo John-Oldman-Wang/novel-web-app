13c16174a835a44a8ba39f3a13cc5d511a8ba572
wanglin <mag1c_shen_09@foxmail.com>
2017-12-08 00:30:24 +0800

    change main for single page
    change main for single page

--- www/main.js
+++ www/main.js
@@ -6,16 +6,14 @@ const { BrowserRouter, Route, Link } = require('react-router-dom')
 
 const Index=require('./component/index.js')
 const About=require('./component/about.js')
+const Novel=require('./component/novel.js')
+const Search=require('./component/search.js')
 const Progress=require('./component/progress.js')
 
 class App extends Component{
     render(){
         return (
             <div>
-                <ul>
-                    <Link to='/'>index</Link> 
-                    <Link to='/about'>about</Link> 
-                </ul>
                 {this.props.children}
             </div>
         )
@@ -23,17 +21,12 @@ class App extends Component{
 }
 
 
+window.p1 = ReactDom.render(<Progress pro={0} />, document.getElementById("progressBar"))
 ReactDom.render(<BrowserRouter>
     <App>
         <Route exact  path='/' component={Index}></Route>
+        <Route path='/novel' component={Novel}></Route>
+        <Route path='/search' component={Search}></Route>
         <Route path='/about' component={About}></Route>
     </App>
 </BrowserRouter>,document.getElementById('content'))
-
-var p1=ReactDom.render(<Progress />,document.getElementById("progressBar"))
-var p2=ReactDom.render(<Progress />,document.getElementById("footer"))
-
-setTimeout(() => {
-    p1.goto(69)
-    console.log(p1==p2)
-}, 3000);
