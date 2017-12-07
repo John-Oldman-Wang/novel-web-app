8abd60687b4beb2285df0daf6781947661774628
wanglin <mag1c_shen_09@foxmail.com>
2017-12-08 00:25:25 +0800

    remove hot-server
    remove hot-server

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,10 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './www/index.html' }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"development"'
-        }),
-        new webpack.NoErrorsPlugin()
+        //new webpack.HotModuleReplacementPlugin(),
+        //new webpack.DefinePlugin({
+        //     'process.env.NODE_ENV': '"production"'
+        // }),
+        //new webpack.NoErrorsPlugin()
     ]
 };
