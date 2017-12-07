cb98eafa9463da4939b822b480a969ca2fe03740
wanglin <mag1c_shen_09@foxmail.com>
2017-12-08 00:26:28 +0800

    optimizate animition
    optimizate animition

--- www/component/progress.js
+++ www/component/progress.js
@@ -8,15 +8,30 @@ class Progrees extends Component {
             pro:0.5,
             height:'5px'
         },props)
+        this.state.show=this.state.pro!=1
     }
     goto(value){
-        
         if(value>1){
-            value=value/100
+            value=(value%101)/100
         }
-        this.setState({
-            pro:value
-        })
+        if(!this.state.show){
+            this.setState({
+                show:!!1,
+                pro:value
+            })
+        }else{
+            this.setState({
+                pro:value
+            })
+        }
+        if(value==1){
+            setTimeout(() => {
+                this.setState({
+                    show:false,
+                    pro:0
+                })
+            }, 1000);
+        }
     }
     render() {
         return (
@@ -28,7 +43,8 @@ class Progrees extends Component {
                         height:this.state.height,
                         width: this.state.pro*100 +"%",
                         backgroundColor:this.state.color,
-                        transition:'all 0.5s ease-in-out'
+                        transition:this.state.show?'all 0.5s ease-in':'none',
+                        display: this.state.show ?'block':'none'
                     }}
                 >
                 </div>
