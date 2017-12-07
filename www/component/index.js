b36e518c9577c87c31a1a4ef40270a78149cccd8
wanglin <mag1c_shen_09@foxmail.com>
2017-12-08 00:25:37 +0800

    index compenent
    index compenent

--- www/component/index.js
+++ www/component/index.js
@@ -1,25 +1,62 @@
 const React = require('react');
 const { Component } = require('react')
+const { Link } =require('react-router-dom')
+const Search = require('./search.js')
 class Index extends Component {
     constructor(props){
         super(props)
+        console.log(this)
         this.state={
-            text:"asdas"
+           novels:[] 
         }
+        window.p1.goto(50)
+        var xhr = new XMLHttpRequest()
+        xhr.open('GET', '/index', true)
+        xhr.setRequestHeader('x-response-type', 'multipart')
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                var json = JSON.parse(xhr.responseText)
+                this.setState({
+                    novels: json.novels
+                })
+                window.p1.goto(100)
+            }else{
+                window.p1.goto(80)
+            }
+        }
+        xhr.send()
     }
-    hander(e){
-        this.setState({
-            text:e.target.value
-        })
+    componentWillMount(){
     }
     render() {
-        console.log('render')
-        return (
-            <div>
-                <h1>index</h1>
-                <input type="text" value={this.state.text} onChange={(e)=>{this.hander(e)}} />
-            </div>
-        );
+        if(this.state.novels.length==0){
+            return (
+                <div>
+                    <Search/>
+                    加载中
+                </div>
+            );
+        }else{
+            return(
+                <div>
+                    <Search />
+                    <ul>
+                        {this.state.novels.map(novel=>{
+                            return (
+                                <li key={novel._id}>
+                                    <Link  to={'/novel?v='+novel._id}>  
+                                    <h1>{novel.title}</h1>
+                                    <img src={novel.image} />
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            )
+        }
+    }
+    componentDidMount(){
     }
 }
 module.exports = Index
