b273b31d5cb19c7a394a2199a18fe84298558c62
wanglin <mag1c_shen_09@foxmail.com>
2017-12-13 16:24:50 +0800

    1.0.3 # prefect the function of search
    1.0.3 # prefect the function of search

--- app.js
+++ app.js
@@ -65,7 +65,9 @@ app.get('/test',function(req,res){
 })
 app.get('/search',function(req,res,next){
     if (req.headers['x-response-type'] == 'multipart') {
-        Novel.find({title:req.query.key}, function (err, novels) {
+        var key=req.query.key.replace()
+        var reg=new RegExp(key.split('').join('.*'),'g')
+        Novel.find({title:reg},{href:0,meta:0}).limit(20).exec(function (err, novels) {
             res.json(novels)
         })
     } else {
