9f685f952d327c57fa109c5221fa49eb350ee30d
wanglin <mag1c_shen_09@foxmail.com>
2018-01-17 16:13:39 +0800

    1.3.2 # use materialize write index page
    1.3.2 # use materialize write index page

--- web/component/index.js
+++ web/component/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-var xhr = new XMLHttpRequest()
+
+import {CardPanel , CardTitle , Card, Autocomplete, Col, Row, Input, Navbar, NavItem, Icon, Button } from 'react-materialize'
+
+
 import Search from './searchHeader.js'
+var xhr = new XMLHttpRequest()
 export class Index extends Component {
     constructor(props){
         super(props)
         this.state={
            novels:[] 
         }
+        // console.log()
+        window.index=this
         window.p1.goto(50)
         xhr.open('GET', '/index?pbj=1', true)
         xhr.setRequestHeader('x-response-type', 'multipart')
@@ -27,39 +33,43 @@ export class Index extends Component {
     componentWillMount(){
     }
     render() {
-        if(this.state.novels.length==0){
-            return (
-                <div>
-                    加载中
-                </div>
-            );
-        }else{
-            return(
-                <div>
-                    <Search />
-                    <ul className="novel-list">
-                        {this.state.novels.map(novel=>{
-                            return (
-                                <li key={novel._id}>
-                                    <Link to={'/novel?v=' + novel._id} rel={novel.title}>
+        return(
+            <div>
+                <Navbar brand='无限小说' right>
+                    <NavItem href="javascript:void(0)" onClick={()=>{
+                        this.props.history.push('/search?key=')
+                    }}>
+                        <Icon left>search</Icon>搜索
+                    </NavItem>
+                    {/* <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+                    <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+                    <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem> */}
+                </Navbar>
+                <ul className="row">
+                    {this.state.novels.map(novel=>{
+                        return (
+                            <li className="col s4" key={novel._id}>
+                                {/* <Card > */}
+                                <Link to={'/novel?v=' + novel._id} rel={novel.title}>
+                                    <CardTitle style={{fontSize: "0px"}} reveal image="">
                                         <div className="novel-img">
                                             <div className="imgbox"></div>
                                             <div className="img">
-                                            <img  src={novel.image} alt={novel.title}/>
+                                                <img src={novel.image} alt={novel.title} />
                                             </div>
                                         </div>
-                                        <div className="novel-message">
+                                        <div style={{ fontSize: "15px" }} className="novel-message">
                                             <h1 className="novel-title">{novel.title}</h1>
                                             <h3 className="novel-author">{novel.author}</h3>
                                         </div>
-                                    </Link>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            )
-        }
+                                    </CardTitle>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        )
     }
     componentDidMount(){
     }
