7184174f29b47ce72e13e81fcf0a185120bfce9c
johnWang <wanglin@zhiwen.me>
2018-06-20 18:53:43 +0800

    #3.0.3 添加了注册的ui界面

---
+++ web/containers/signUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUp from '../pages/signUp.jsx'
+
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state, props) => {
+    return {}
+}
+const mapDispatchToProps = (dispatch, props) => {
+    return {}
+}
+exports.SignUp = connect(mapStateToProps, mapDispatchToProps)(SignUp)
