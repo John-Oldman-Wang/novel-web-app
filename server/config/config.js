0e308b40bc74a6e8b498e128e83f3834a94cfe83
王琳 <mag1c_shen_09@foxmail.com>
2019-01-13 16:57:38 +0800

    删除无用文件

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    dbUrl: "mongodb://localhost:27017/novel",
-    domain:'',
-    siteName:'',
-    port: 8080,
-    versionOfMainjs:'',
-    versionOfMain:'',
-    key: 'novel'
-}
