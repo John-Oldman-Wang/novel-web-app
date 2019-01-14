f6b5d7890cf06b9470c86b7826ba8b021f7470ab
王琳 <mag1c_shen_09@foxmail.com>
2019-01-14 23:15:58 +0800

    删除旧版本代码

--- web/plugin/formSearch.js
+++
@@ -1,9 +0,0 @@
-module.exports=function(search){
-    if(search==''){
-        return {}
-    }else{
-        var str=search.replace(/^\?/,'').replace(/=/g,'":"').replace(/\&/g,'","')
-        str='{"'+str+'"}'
-        return JSON.parse(str)
-    }
-}
